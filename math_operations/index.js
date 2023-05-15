149a0b97677b6b1f00f3b33b01b985cf0d5e0410
Vlad Popov <vladpopov20030201@gmail.com>
2023-05-15 18:09:11 +0300

    hometask_3

---
+++ math_operations/index.js
@@ -0,0 +1,9 @@
+let a = +prompt("Введіть перше число");
+let b = +prompt("Введіть друге число");
+let sum = a + b;
+let diff = a - b;
+let prod = a * b;
+let frac = a / b;
+alert(
+    `//Користувач ввів ${a} і ${b}: \n ${a} + ${b} = ${sum} \n ${a} - ${b} = ${diff} \n ${a} * ${b} = ${prod} \n ${a} / ${b} = ${frac}`
+);
