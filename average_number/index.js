5c591b155b525a9e729ed450f39d91cff1b680b0
Vlad Popov <vladpopov20030201@gmail.com>
2023-09-04 10:27:48 +0300

    average number

---
+++ average_number/index.js
@@ -0,0 +1,5 @@
+let num_1 = +prompt('Введи 1 число');
+let num_2 = +prompt('Введи 2 число');
+let num_3 = +prompt('Введи 3 число');
+let average = (num_1 + num_2 + num_3) / 3;
+alert(`Середнє арифметичне = ${average}`); 
